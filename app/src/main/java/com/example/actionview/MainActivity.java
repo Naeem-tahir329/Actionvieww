2e1be92ac501fc3eb5926ff9062b00d8865f3536
Naeem.Tahir <Naeem.tahirr329@gmail.com>
2022-01-07 21:23:29 +0500

    add Toast

--- app/src/main/java/com/example/actionview/MainActivity.java
+++ app/src/main/java/com/example/actionview/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Avtivity Created", Toast.LENGTH_SHORT).show();
     }
     @SuppressLint("ResourceType")
     @Override
